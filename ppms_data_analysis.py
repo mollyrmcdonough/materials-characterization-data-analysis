30c3d479f6e3a5c1115fbfcda6eafcd4a2df656c
Molly McDonough <mrm6464@psu.edu>
2023-05-02 12:47:58 -0400

    Changes to be committed: 	new file:   Example Data/XRD Data/XRD4_MBE1-230331A-MS_33.9475 Omega_Omega Peaks.csv 	new file:   Example Data/XRD Data/XRD4_MBE1-230331A-MS_33.9475 Omega_export.csv 	new file:   Example Data/XRD Data/XRD4_MBE1-230331A-MS_Gonio Pixcel_Gonio Peaks.csv 	new file:   Example Data/XRD Data/XRD4_MBE1-230331A-MS_Gonio Pixcel_export.csv 	modified:   ppms_data_analysis.py 	modified:   transport_analysis.py 	new file:   xrd_analysis.py

--- ppms_data_analysis.py
+++ ppms_data_analysis.py
@@ -1,7 +1,7 @@
 import transport_analysis as ppms
 # Define the file path
-file_path_RvsT = "TRP4_MBE1-230323A-MS_RT_300_2K_data.csv"
-file_path_RvsH = "TRP4_MBE1-230323A-MS_RvsH_20K_data_T=20K.csv"
+file_path_RvsT = "Example Data\Transport Data\TRP4_MBE1-230323A-MS_RT_300_2K_data.csv"
+file_path_RvsH = "Example Data\Transport Data\TRP4_MBE1-230323A-MS_RvsH_20K_data_T=20K.csv"
 
 [temp_k_RvsT, rxx_RvsT, rxy_RvsT] = ppms.get_R_vs_T(file_path_RvsT)
 [temp_k_RvsH, bfield_T_RvsH, rxx_RvsH, rxy_RvsH] = ppms.get_R_vs_H(file_path_RvsH)
